1020eb9cc0b609f2f327ce4961e9bc807a0d50f8
José Padilla <jpadilla@webapplicate.com>
2014-08-24 10:42:33 -0400

    Customizable CORS headers

--- index.js
+++ index.js
@@ -3,16 +3,19 @@ var http = require('http'),
     url = require('url');
 
 var port = process.env.PORT || 8000,
-    proxyURL = process.env.PROXY_URL || 'http://registry.npmjs.org:80/';
+    proxyURL = process.env.PROXY_URL || 'http://registry.npmjs.org:80/',
+    allowOrigin = process.env.ALLOW_ORIGIN || '*',
+    allowMethods = process.env.ALLOW_METHODS || '*',
+    allowHeaders = process.env.ALLOW_HEADERS || 'X-Requested-With'
 
 http.createServer(function (req, res) {
   var r = request(url.resolve(proxyURL, req.url));
 
   // Add CORS Headers
   r.on('response', function(_r) {
-    _r.headers['Access-Control-Allow-Origin'] = '*';
-    _r.headers['Access-Control-Allow-Methods'] = '*';
-    _r.headers['Access-Control-Allow-Headers'] = 'X-Requested-With';
+    _r.headers['Access-Control-Allow-Origin'] = allowOrigin;
+    _r.headers['Access-Control-Allow-Methods'] = allowMethods;
+    _r.headers['Access-Control-Allow-Headers'] = allowHeaders;
   });
 
   // Stream the response
