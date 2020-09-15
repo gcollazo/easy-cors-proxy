1e19cd66d5b9e129eacd2627682c8754fa73c885
Blaster Niceshot <34875784+BlasterNiceshot@users.noreply.github.com>
2020-09-15 16:48:45 -0400

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ http.createServer(function (req, res) {
   if (hostname.endsWith('.fandom.com') || hostname.endsWith('.wikia.org')) {
     var apiPath = '/api/v1';
     var reqUrl = req.url.startsWith(apiPath) ? req.url : apiPath + req.url;
-    var r = request(proxyURL + reqUrl);
+    var r = request(new URL(reqUrl, proxyURL));
 
     // Add CORS Headers
     r.on('response', function(_r) {
